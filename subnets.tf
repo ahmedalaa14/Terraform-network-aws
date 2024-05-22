27269f7829a55d5bb5096f4545a449621056caee
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-05-22 18:29:31 +0300

    update subnets

--- subnets.tf
+++ subnets.tf
@@ -1,14 +1,14 @@
 
 resource "aws_subnet" "public1" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = var.public_subnet_1_cidr
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.public_subnet_1_cidr
   map_public_ip_on_launch = "true"
 }
 
 
 resource "aws_subnet" "public2" {
-  vpc_id     = aws_vpc.myvpc.id
-  cidr_block = var.public_subnet_2_cidr
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = var.public_subnet_2_cidr
   map_public_ip_on_launch = "true"
 }
 
